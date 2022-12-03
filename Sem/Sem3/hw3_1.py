fe6fa0895e8c15cf8acd8a32c4c78f78637c1f62
LightningNight1 <zoebennnettt@gmail.com>
2022-12-03 12:44:59 +0300

    Done

--- Sem/Sem3/hw3_1.py
+++ Sem/Sem3/hw3_1.py
@@ -1,16 +1,13 @@
 # 1) Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 
-def my_func(*args):
+def my_func(arg1, arg2):
     try:
-        arg1 = int(input('Enter the integer arg1: '))
-        arg2 = int(input('Enter the integer arg2: '))
-        res = arg1 / arg2
-    except ValueError:
-        return 'Invalid value'
+        res = arg1/arg2
+        return res
     except ZeroDivisionError:
         return '0 can never be a divisor of any number'
-    return res
 
 
-print(my_func())
+print(my_func(int(input('Enter the integer arg1: ')),
+      int(input('Enter the integer arg2: '))))
